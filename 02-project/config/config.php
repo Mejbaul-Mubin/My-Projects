c9b517e4c62ce38e82f9f8b2fad9a516ff2c75ad
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-04 09:50:12 +0600

    Fr

--- 02-project/config/config.php
+++ 02-project/config/config.php
@@ -7,9 +7,3 @@ define("PASSWORD", "");
 
 $conn = new PDO("mysql:host=" . HOST . ";dbname=" . DBNAME . ";", USERNAME, PASSWORD);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-// if ($conn == true) {
-//     echo "connection is a success";
-// } else {
-//     echo "error";
-// }
