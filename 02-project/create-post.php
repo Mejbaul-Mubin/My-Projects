ce8295cc373f3d8cb7fc2c649094bb21788ad669
Mejbaul Mubin <mejbaulmubin@my.uopeople.edu>
2024-09-04 08:21:49 +0600

    Front end almost complite

--- 02-project/create-post.php
+++ 02-project/create-post.php
@@ -4,19 +4,19 @@
 
 
 
-            <form>
+            <form method="post" action="create-post.php">
 
                 <div class="mb-3">
                   <label for="exampleInputEmail1" class="form-label">Title </label>
-                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  <input type="text" name="title" placeholder="write title" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                 </div>
 
                 <div class="mb-3">
                   <label for="exampleInputPassword1" class="form-label">Post Author</label>
-                  <input type="password" class="form-control" id="exampleInputPassword1">
+                  <input type="text" name="post_author" placeholder="write author name" class="form-control" id="exampleInputPassword1">
                 </div>
 
-                <select class="form-select mb-5 mt-5" aria-label="Default select example">
+                <select name="category" class="form-select mb-5 mt-5" aria-label="Default select example">
                     <label class="form-label">Choose Category</label>
 
                     <option selected>Choose Category</option>
@@ -25,7 +25,7 @@
                     <option value="3">Programming</option>
                 </select>
 
-                <button type="submit" class="btn btn-primary w-100">Submit</button>
+                <button name="submit" type="submit" class="btn btn-primary w-100">Submit</button>
 
               </form>
 
